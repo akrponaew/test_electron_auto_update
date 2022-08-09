741d7c25476e43fb894f84e3b0040557931a7bd8
Akrapon Panyawattanakit <akarapon.pan@everapp.io>
2022-08-09 14:49:41 +0700

    1.0.9

--- main.js
+++ main.js
@@ -6,6 +6,15 @@ const {
   dialog,
 } = require("electron");
 
+const server = "https://test-electron-auto-update-akrponaew.vercel.app";
+const url = `${server}/update/${process.platform}/${app.getVersion()}`;
+
+autoUpdater.setFeedURL({ url });
+
+setInterval(() => {
+  autoUpdater.checkForUpdates();
+}, 5000);
+
 let mainWindow;
 
 function createWindow() {
@@ -53,29 +62,49 @@ app.on("activate", function () {
   }
 });
 
-ipcMain.on("app_version", (event) => {
-  event.sender.send("app_version", { version: app.getVersion() });
-});
-
-autoUpdater.on("update-available", () => {
+autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
   const dialogOpts = {
     type: "info",
     buttons: ["Restart", "Later"],
     title: "Application Update",
-    message: app.getVersion(),
-    detail: "from update-available",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
   };
 
-  // win.once("ready-to-show", () => {
-  dialog.showMessageBox(dialogOpts).then((returnValue) => {});
-  // });
-
-  mainWindow.webContents.send("update_available");
-});
-autoUpdater.on("update-downloaded", () => {
-  mainWindow.webContents.send("update_downloaded");
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
 });
 
-ipcMain.on("restart_app", () => {
-  autoUpdater.quitAndInstall();
+autoUpdater.on("error", (message) => {
+  console.error("There was a problem updating the application");
+  console.error(message);
 });
+
+// ipcMain.on("app_version", (event) => {
+//   event.sender.send("app_version", { version: app.getVersion() });
+// });
+
+// autoUpdater.on("update-available", () => {
+//   const dialogOpts = {
+//     type: "info",
+//     buttons: ["Restart", "Later"],
+//     title: "Application Update",
+//     message: app.getVersion(),
+//     detail: "from update-available",
+//   };
+
+//   // win.once("ready-to-show", () => {
+//   dialog.showMessageBox(dialogOpts).then((returnValue) => {});
+//   // });
+
+//   mainWindow.webContents.send("update_available");
+// });
+// autoUpdater.on("update-downloaded", () => {
+//   mainWindow.webContents.send("update_downloaded");
+// });
+
+// ipcMain.on("restart_app", () => {
+//   autoUpdater.quitAndInstall();
+// });
